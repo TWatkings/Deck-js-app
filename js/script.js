ab4eb97724a7ae1a3d7bdd628c6eeef20077b6d6
TWatkings <106554653+TWatkings@users.noreply.github.com>
2022-06-08 00:26:57 -0400

    Array of pokemons, their height, and type.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
-alert("Hello World");
-let favorFood = "pizza";
-document.write(favorFood);
+//Array of pokemons, their height, and type.
+let pokemonList = ['Venusar', ' Onix', ' Alakazam', ' Mienshao' ];
+let height = [2, 8.8, 1.5, 1.4];
+let type = [[' monster', ' grass'], [' mineral'], [' humanLike'], [' Field', ' humaLike']];
+document.write(type);
