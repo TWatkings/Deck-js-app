a81d972a2b86d9839cefec35e98c338e69411da6
TWatkings <106554653+TWatkings@users.noreply.github.com>
2022-06-09 13:18:20 -0400

    updated code

--- js/script.js
+++ js/script.js
@@ -8,25 +8,17 @@ let pokemonList = [
     {name:'Mienshao', height:1.4, type:[' Field', ' humaLike']}
 
 ];
-for (let i = 0; i <= pokemonList.length; i++) {
-    if (pokemonList[i].height > 5) {
-        document.write(pokemonList[i].name)
-        document.write(pokemonList[i].height + ' wow thats BIG!')
-    }
-    document.write(pokemonList[i].name + ' height: '); 
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + '(height):'); 
 
     document.write(pokemonList[i].height);
+    if (pokemonList[i].height > 5) {
+        
+        document.write(pokemonList[i].height + ' wow thats BIG!');
+    }
+  
 
     
-};
+}
 
-// for (let i=0; i < person.length; i++){
-//     if (person[i].age <19 && person[i].age >13){
-//       console.log(person[i].name + " is a teenager");
-//     }else if (person[i].age <13){
-//       console.log(person[i].name + " is a child");
-//     }else {
-//       console.log(person[i].name + " is an adult");
-//     }
-//   }
 
