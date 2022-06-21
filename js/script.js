6e557245d531408f6b0b6aae388f259a51debd99
TWatkings <106554653+TWatkings@users.noreply.github.com>
2022-06-20 20:49:26 -0400

    addEventListener on button 'click'

--- js/script.js
+++ js/script.js
@@ -1,64 +1,56 @@
-//Array of pokemons, their height, and type.
-let pokemonRepository = (function() {
-  let pokemonList = [
-   
-   
+let pokemonRepository = (function () {
+  let repository = [
     {name:'Venusar', height:2, type:['monster','grass']},
     {name:'Onix', height:8.8, type:[' mineral']},
     {name:'Alakazam', height:1.5, type:[' humanLike']},
-    {name:'Mienshao', height:1.4, type:[' Field', ' humaLike']}
+    {name:'Mienshao', height:1.4, type:[' Field', ' humaLike']},
+  ];
 
-];
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon
+    ) {
+      repository.push(pokemon);
+    } else {
+      console.log("pokemon is not correct");
+    }
+  }
+  function getAll() {
+    return repository;
+  }
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
 
-function add(metapod) {
+  function addListItem(pokemon){
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+    
+    
+    button.addEventListener('click', function (pokemon) {
+      showDetails(pokemon);
+    })
+  }
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem
+  };
   
-  pokemonList.push(metapod);
-}
-
-function getAll() {
- return pokemonList;
-}
-return {
-  add:add, 
-  getAll:getAll
-}
-
 })();
 
-console.log(pokemonRepository.add());
-pokemonRepository.add({name: 'metapod'});
+pokemonRepository.add({ name: "Matapod", height: 2.4, types: ["bug"] });
+
 console.log(pokemonRepository.getAll());
 
-
-
-
-
-pokemonRepository.getAll().forEach(function(pokemonArr) {
-  document.write('<p>' + pokemonArr.name + '</p>' + 'Height ' + pokemonArr.height +'' + '<p>' + pokemonArr.type + '</p>' );
- 
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
